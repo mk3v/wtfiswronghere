8fb3e5be96a580caa2f6c73583e2770fc66afdc0
mk3v <mohitkhanna3vinfinate@gmail.com>
2025-11-02 10:05:45 -0800

    fix for challenge 8

--- 08_challenge/08_challenge.py
+++ 08_challenge/08_challenge.py
@@ -12,8 +12,8 @@ For all integers between 1 and 99 (include both):
 class Fizz_Buzz:
     "Class to implement FizzBuzz for multiples of 3 and 5"
 
-    def fizzbuzz(max_num):
-        "This method implements FizzBuzz"
+    def fizzbuzz(self, max_num):
+        #"This method implements FizzBuzz"
         
         # adding some redundant declarations on purpose
         # we will make our script 'tighter' in one of coming exercises
