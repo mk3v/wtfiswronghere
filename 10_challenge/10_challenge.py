56215da7565151b23f2851b32e9363388371cb6b
mk3v <mohitkhanna3vinfinate@gmail.com>
2025-11-02 10:08:11 -0800

    fix for challenge 10

--- 10_challenge/10_challenge.py
+++ 10_challenge/10_challenge.py
@@ -10,6 +10,17 @@ For all integers between 1 and 99 (include both):
 """
 import fizzbuzz
 
+def fixbuzz(maxNum):
+
+    for i in range(1, maxNum):
+        if i % 3 == 0 and i %5 == 0:
+            print(i, 'fizzbuzz')
+        elif i % 3 == 0:
+            print(i, 'fizz')
+        elif i % 5 == 0:
+            print(i, 'buzz')
+
+
 #----START OF SCRIPT
 if __name__=='__main__':
     fizzbuzz(100)
