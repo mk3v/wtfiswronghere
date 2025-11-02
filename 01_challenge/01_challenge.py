90b1c059acea9df7ebe564adaf3ae86268cf5c46
mk3v <mohitkhanna3vinfinate@gmail.com>
2025-11-02 09:05:21 -0800

    fix for challenge 1

--- 01_challenge/01_challenge.py
+++ 01_challenge/01_challenge.py
@@ -23,8 +23,8 @@ def fizzbuzz(max_num):
     for i in range(1,max_num):
         # % or modulo division gives you the remainder 
         if i%num1==0 and i%num2==0:
-            print(i,three_mul+five_mul)
-        elif i%num1=0:
+            print(i,three_mul+five_mul, "Multiple of 3 and 5")
+        elif i%num1==0:
             print(i,three_mul)
         elif i%num2==0:
             print(i,five_mul)
